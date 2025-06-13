60cc6d4cd44524c7309e1988b02d860320c94ce5
sayed.polin <sayed.polin@brotecs.com>
2025-06-13 21:23:51 +0600

    release v0.1.1

--- agent_python/app/controllers/auth_controller.py
+++
@@ -1,4 +0,0 @@
-"""
-Auth functionality has been moved to WebSocket events.
-See socket.py for the implementation of check_authority.
-"""
--- agent_python/app/routes/__init__.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-from .socket import init_socketio
-
-def init_routes(app: Flask):
-    # Initialize socket routes only
-    socketio = init_socketio(app)
-    return socketio
